d92f8d63b9c24c91c792142d72f71d01a36e384b
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-06-22 15:29:42 +0530

    Add initial JavaScript files for variables, data types, student records, and todo functionality

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123
+let accessName = "aru"
+
+{
+var acessRight = "ragad"
+}
+// accountId = 33
+accessName = "love"
+var acessRight = "rag"
+console.table([accountId,accessName,acessRight]);
