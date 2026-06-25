e1334fd9b16536794f43ca71474e164cc9487134
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-06-26 02:42:49 +0530

    Add initial functions.js with print and object handling examples

---
+++ basics3/functions.js
@@ -0,0 +1,12 @@
+function print(){
+    console.log("Hello World")
+}
+const user ={
+    name: "arush",
+    key: "lock"
+}
+function objt(anyobject){
+    console.log(`user is name ${anyobject.name}`)
+
+}
+objt(user)
