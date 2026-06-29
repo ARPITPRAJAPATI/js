12bd119bc1c20e97cb27373e0169b7200f1c7415
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-06-29 17:49:20 +0530

    Refactor functions.js to comment out unused functions and update calculateCartPrice to increment input values

--- basics3/functions.js
+++ basics3/functions.js
@@ -1,12 +1,16 @@
-function print(){
-    console.log("Hello World")
-}
-const user ={
-    name: "arush",
-    key: "lock"
-}
-function objt(anyobject){
-    console.log(`user is name ${anyobject.name}`)
+// function print(){
+//     console.log("Hello World")
+// }
+// const user ={
+//     name: "arush",
+//     key: "lock"
+// }
+// function objt(anyobject){
+//     console.log(`user is name ${anyobject.name}`)
 
+// }
+// objt(user)
+function calculateCartPrice(...num1){
+    return num1;
 }
-objt(user)
+console.log(calculateCartPrice(1,2,3,4,5,6,7,8,9,10))
