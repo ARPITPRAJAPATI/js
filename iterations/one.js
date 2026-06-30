3c63850ec3ba6d5f27c7b8e76493b524b327af27
ARPITPRAJAPATI <arpitprajapati2005@gmail.com>
2026-06-30 17:38:20 +0530

    loops

---
+++ iterations/one.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// const fruits = ["Apple", "Banana", "Mango"];
+
+// for (const fruit of fruits) {
+//     console.log(fruit);
+// }
+// const name = "Arpit";
+
+// for (const ch of name) {
+//     console.log(ch);
+// }
+// const student = {
+//     name: "Arpit",
+//     age: 20,   
+//     city: "Ghaziabad"
+// };
+
+// for (const key in student) {
+//     console.log(key);
+// }
+
+// break and continue 
+
+for(let index = 1; index <=20;index++){
+
+    if(index == 5){
+        console.log(`Detected 5`)
+        // break;
+        continue;
+    }
+    console.log(`value of i is ${index}`)
+}
